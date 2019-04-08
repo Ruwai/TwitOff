74a7d24d984f46e347052cd20e14772e07eaeeb3
Ruwai <chrisl6uie@gmail.com>
2019-04-08 10:22:12 -0700

    .

---
+++ twitoff/app.py
@@ -0,0 +1,18 @@
+'''
+Main Application and routing logic for TwitOff
+'''
+
+from flask import Flask
+
+def create_app():
+    '''
+        Create and configure an instance of the Flask application.
+    '''
+
+    app = Flask(__name__)
+    @app.rojute('/')
+    def root():
+        return 'Welcome to TwitOff!'
+
+    return app
+
